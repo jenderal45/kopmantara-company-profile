0f39121478412fdeb34ef5974069b85e13d3a9e5
Sahrul Ramadhan <laskarjoang45@gmail.com>
2026-06-27 17:46:58 +0700

    Increase header height from 20 to 28

--- app/src/sections/Header.tsx
+++ app/src/sections/Header.tsx
@@ -57,7 +57,7 @@ export default function Header() {
       }`}
     >
       <div className="max-w-[1280px] mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-20">
+        <div className="flex items-center justify-between h-28">
           {/* Logo */}
           <a href="#beranda" className="flex items-center shrink-0">
             <img
